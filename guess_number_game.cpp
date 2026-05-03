810fb88ce19681e39c1696d61024df7d6499d71e
Maryam Emad <cit-ai-25050@uofallujah.edu.iq>
2026-05-03 09:14:05 -0700

    Add C++ project source code

---
+++ guess_number_game.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+struct Player {
+    string name;
+    int attemptsUsed;
+};
+
+
+void checkGuess(int guess, int secret) {
+    if (guess == secret) {
+        cout << "Correct!\n";
+    }
+    else if (guess > secret) {
+        cout << "Lower\n";
+    }
+    else {
+        cout << "Higher\n";
+    }
+}
+
+int main() {
+    srand(time(0));
+
+    int secret = rand() % 100 + 1;
+    int guess;
+    int attempts = 5;
+
+
+    Player player;
+    cout << "Enter your name: ";
+    cin >> player.name;
+    player.attemptsUsed = 0;
+
+
+    int guesses[5];
+
+    cout << "Guess the number (1 - 100)\n";
+
+    while (attempts > 0) {
+        cout << "Attempts left: " << attempts << endl;
+        cin >> guess;
+
+        guesses[player.attemptsUsed] = guess;
+        player.attemptsUsed++;
+
+        checkGuess(guess, secret);
+
+        if (guess == secret) {
+            break;
+        }
+
+        attempts--;
+    }
+
+    if (guess != secret) {
+        cout << "You lost! The number was: " << secret << endl;
+    }
+
+
+    cout << "\nYour guesses were: ";
+    for (int i = 0; i < player.attemptsUsed; i++) {
+        cout << guesses[i] << " ";
+    }
+
+    cout << "\nPlayer: " << player.name << endl;
+    cout << "Attempts used: " << player.attemptsUsed << endl;
+
+    return 0;
+}
